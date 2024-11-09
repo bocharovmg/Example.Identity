56f1b013228f52701da6d610a1796d4cf5c9975b
Mikhail <bgmihail@list.ru>
2024-11-09 14:07:08 +0700

    Added VerificationStateTypes table

---
+++ Db/Scripts/DataMigrations/0006. Cteate verification state types.sql
@@ -0,0 +1,47 @@
+﻿/** VERIFICATION STATE TYPES */
+
+DECLARE
+	@VerificationStateTypeId_Password INT = 1
+	, @VerificationStateTypeDescription_Password NVARCHAR(50) = N'Password'
+	, @VerificationStateTypeId_Email INT = 2
+	, @VerificationStateTypeDescription_Email NVARCHAR(50) = N'Email'
+	, @VerificationStateTypeId_AlternativeEmail INT = 3
+	, @VerificationStateTypeDescription_AlternativeEmail NVARCHAR(50) = N'AlternativeEmail'
+
+MERGE [user].[VerificationStateTypes] tgt
+USING (
+	SELECT
+		[Id] = @VerificationStateTypeId_Password
+		, [Description] = @VerificationStateTypeDescription_Password
+
+	UNION
+	SELECT
+		[Id] = @VerificationStateTypeId_Email
+		, [Description] = @VerificationStateTypeDescription_Email
+
+	UNION
+	SELECT
+		[Id] = @VerificationStateTypeId_AlternativeEmail
+		, [Description] = @VerificationStateTypeDescription_AlternativeEmail
+) src
+ON (
+	tgt.Id = src.Id
+)
+WHEN NOT MATCHED BY TARGET
+	THEN
+		INSERT
+		(
+			[Id]
+			, [Description]
+		)
+		VALUES
+		(
+			src.Id
+			, src.Description
+		)
+WHEN MATCHED
+	THEN
+		UPDATE
+		SET
+			tgt.Description = src.Description
+;
---
+++ Db/user/Tables/VerificationStateTypes.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [user].[VerificationStateTypes]
+(
+	[Id] INT NOT NULL,
+    [Description] NVARCHAR(50) NOT NULL, 
+    CONSTRAINT [PK__user__VerificationStateTypes] PRIMARY KEY ([Id])
+)
+
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX [UIX__user__VerificationStateTypes_Description] ON [user].[VerificationStateTypes] ([Description])
